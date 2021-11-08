69d9bb20d7c61475f7146136525a765fdec9f335
Jorge Rangel <jorgelrgarduno@gmail.com>
2021-11-08 11:29:54 -0600

    2 Delimitación del area de movimiento de personaje sin friccion

--- js/main.js
+++ js/main.js
@@ -7,10 +7,11 @@ const ctx = $canvas.getContext("2d");
 //Variables  Globales.
 let intervalId;
 let frames = 0;
-const friccion = 0.9;
+// const friction = 1.09;
 
 
 // Definimos las clases.
+//1Clase Global
 class GameAsset {
     constructor(x, y, width, height, img) {
         this.x = x;
@@ -29,6 +30,8 @@ class GameAsset {
 }
 
 
+
+//2 Clase de mi escenario
 class Board extends GameAsset {
     constructor(x, y, width, height, img) {
         super(x, y, width, height, img);
@@ -39,32 +42,73 @@ class Board extends GameAsset {
 	}
 }
 
-
+//3Clase de mi personaje 
 class Character extends GameAsset {
     constructor(x, y, width, height, img) {
         super(x, y, width, height, img);
-        this.move = 8;
+         //se utiuliza con el primer metedo para mover las funciones checar si se quita o se queda
+        this.move = 15;
         this.vx = 0;
         this.vy = 0;
 
     }
 
     draw() {
+        // Se delimito el area inferior en la que el soldado va a poder moverse    
+        if(this.y > $canvas.height - 78){
+            this.y = $canvas.height- 78;
+        }
+        // Se delimito el area superio en la que el soldado va a poder moverse    
+        if(this.y < 5){
+            this.y = 5;
+        }
+        // Se delimito el area trasera(izquierda) en la que el soldado va a poder moverse    
+        if(this.x > $canvas.width - 270){
+            this.x = $canvas.width- 270;
+        }
+        // Se delimito el area delantera (derecha) en la que el soldado va a poder moverse    
+        if(this.x < 130){
+            this.x = 130;
+        }
+
+        //this.x += this.vx;
+        //vamos a implementar la friccion eje horizontal
+        //this.vx *= friction;
+        //vamos a implementar la friccion eje vertical
+        //this.y += this.vy;
+        //this.vy *= friction;
+
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 	}
-
+    // Metodo para moverse para arriba
     moveUp() {
+        // this.move = 15; se utiuliza con el primer metedo para mover las funciones checar si se quita o se queda
 		this.y -= this.move;
+        //this.vy --;
 	}
+    // Metodo para moverse para abajo
 	moveDown() {
+        // this.move = 15; se utiuliza con el primer metedo para mover las funciones checar si se quita o se queda
 		this.y += this.move;
+        //this.vy ++;
 	}
+    // Metodo para moverse para izquierda
 	moveLeft() {
+        // this.move = 15; se utiuliza con el primer metedo para mover las funciones checar si se quita o se queda
 		this.x -= this.move;
+        //this.vx --;
 	}
+    // Metodo para moverse para derecha
 	moveRight() {
+        // this.move = 15; se utiuliza con el primer metedo para mover las funciones checar si se quita o se queda
 		this.x += this.move;
+        //this.vx ++;
 	}
+
+    /* stop(){
+        this.vx = 0;
+        this.vy = 0;
+    } */
 
 }
 
@@ -75,9 +119,10 @@ const boardImage = "/img/1Background1.jpg"
 const soldierImage = "/img/2Soldado1.png"
 const zombieImage = "/img/3Zombie1.png"
 
-
+//Instancia de escenario
 const board = new Board(0, 0, $canvas.width, $canvas.height, boardImage);
-const solider = new Character(200, $canvas.height / 2, 60, 60, soldierImage) 
+//Instancia de mi personaje
+const solider = new Character(200, $canvas.height / 2, 60, 60, soldierImage); 
  
 // Funciones principales.
 function start() {
@@ -92,7 +137,7 @@ function update() {
     frames++;
     
 
-    //2clearCanvas();
+    //2Limpiar Canvas();
     clearCanvas();
 
 
@@ -114,19 +159,23 @@ function clearCanvas(){
 
 document.onkeydown = (event) => {
     switch (event.key) {
-        case "Enter":
-            //Iniciamos el Juego
+        //Iniciamos el Juego precionando la tecla enter
+        case "Enter":            
             start();
             break;
+        //Funcionalidad de la tecla arriba
         case "ArrowUp":
             solider.moveUp();
             break;
+        //Funcionalidad de la tecla abajo
         case "ArrowDown":
             solider.moveDown();
             break;
+        //Funcionalidad de la tecla izquierda
         case "ArrowLeft":
             solider.moveLeft();
             break;
+        //Funcionalidad de la tecla derecha
         case "ArrowRight":
             solider.moveRight();
             break;
@@ -138,3 +187,10 @@ document.onkeydown = (event) => {
     
 
 //$button.onclick = start();
+
+
+// Cuando alguien deje de precionar la tecla que se detenga 
+
+/* document.onkeyup = () => {
+    solider.stop();
+}; */
