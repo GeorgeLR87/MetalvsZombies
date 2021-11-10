4d250c3f40ca70a7cc8f3a47f5749b5cde92a26e
Jorge Rangel <jorgelrgarduno@gmail.com>
2021-11-10 17:10:56 -0600

    vidas y puntos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Traer los Elementos del DOM (canvas).
 const $canvas = document.querySelector("canvas");
-const $button = document.querySelector("button")
+const $button = document.querySelector(".button")
 const ctx = $canvas.getContext("2d");
 
 
@@ -13,12 +13,17 @@ const friction = 0.85;
 const keys = {};
 // arreglo para los enemigos
 const zombies = [];
+// arreglo para los enemigos
+const zombieBoss = [];
 // arreglo para las balas
 const bullets = [];
 // arreglo para los enemigos
 const powers = [];
 //variable de las vidas
 let lives = 3;
+// const lives = [];
+
+const livesoldier = [];
 //Variable Puntos
 let points = 0;
 
@@ -62,7 +67,6 @@ class Character extends GameAsset {
     }
 
     draw() {
-
         this.x += this.vx;
         //vamos a implementar la friccion eje horizontal
         this.vx *= friction;
@@ -70,7 +74,6 @@ class Character extends GameAsset {
         this.y += this.vy;
         //vamos a implementar la friccion eje vertical
         this.vy *= friction;
-
 
         // Se delimito el area inferior en la que el soldado va a poder moverse
         if(this.y > $canvas.height - 78){
@@ -91,10 +94,6 @@ class Character extends GameAsset {
 
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 	}
-    // Metodo Creado para el impacto con los enemigos.
-    crash() {
-
-    };
     // Metodo para moverse para arriba
     moveUp() {
         this.vy --;
@@ -111,14 +110,12 @@ class Character extends GameAsset {
 	moveRight() {
         this.vx ++;
 	}
-
     // Metodo para detener el personaje
     stop(){
         this.vx = 0;
         this.vy = 0;
     }
     // codigo para generara coliciones
-
     isTouching(obstacle) {
         return (
             // si toca con la parte derecha de nuestro obstaculo
@@ -138,12 +135,10 @@ class Bullet extends GameAsset {
     constructor(x, y, width, height, img) {
         super(x, y, width, height, img);
     }
-
     draw() {
         this.x ++;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
-
     isTouching(obstacle) {
         return (
             // si toca con la parte derecha de nuestro obstaculo
@@ -156,8 +151,6 @@ class Bullet extends GameAsset {
             this.y + this.height > obstacle.y
         )
     }
-
-
 }
 
 //5Clase Enemigos
@@ -165,7 +158,6 @@ class Zombie extends GameAsset {
     constructor(y, width, height, img) {
         super($canvas.width, y, width, height, img);
     }
-
     draw() {
         //Para que salgan de la derecha hacia la izquierda
         this.x--;
@@ -186,7 +178,6 @@ class Power extends GameAsset {
     constructor(x, y, width, height, img) {
         super(x, y, width, height, img);
     }
-
    draw() {
        //Para que caigan
        //this.y++;
@@ -211,6 +202,55 @@ class Power extends GameAsset {
     }
 };
 
+//6Clase Vidas
+class Live extends GameAsset {
+    constructor(x, y, width, height, img) {
+        super(x, y, width, height, img);
+    }
+   draw() {
+    
+        if(lives === 5) {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 50, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 100, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 150, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 200, this.y, this.width, this.height);
+        }
+        else if( lives === 4){
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 50, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 100, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 150, this.y, this.width, this.height);
+        } 
+        else if( lives === 3){
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 50, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 100, this.y, this.width, this.height);
+        } 
+        else if( lives === 2){
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x - 50, this.y, this.width, this.height);
+            
+        }
+        else if( lives === 1){
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+        
+        
+    }
+};
+
+//6Clase Vidas
+class Points extends GameAsset {
+    constructor(x, y, width, height) {
+        super(x, y, width, height, '');
+    }
+   draw() {
+        ctx.fillText(points, 460, 20) 
+    }
+};
+
+
 
 
 // Instancias de las clases.
@@ -220,32 +260,37 @@ const boardImage = "/img/1Background1.jpg";
 const soldierImage = "/img/2Soldado1.png";
 const bulletImage = "/img/4Bullet.png";
 const zombieImage = "/img/3Zombie1.png";
+const zombie2Image = "/img/8Zombie2.png";
 const powerImage = "/img/5HongoPoder.png"
+const liveImage = "/img/7Soldado2.png"
 
 //Instancia de escenario
 const board = new Board(0, 0, $canvas.width, $canvas.height, boardImage);
-//Instancia de mi personaje
+//Instancia de personaje
 const solider = new Character(200, $canvas.height / 2, 70, 70, soldierImage);
-
+//Instancia de live
+const live = new Live(750, 10, 30, 30, liveImage)
+//Instancia de Puntos
+const point = new Points()
 
 
 
 // Funciones principales.
 function start() {
-    if(intervalId) return; // checar
     intervalId = setInterval(() => {
         update();
     }, 1000/60)
 };
 
-function update() {
+
+function update() { 
     //1Calcular el Estado
     frames++;
     generateZombies();
+    generateBossZombie()
     generatePowers();
     checkCollitions();
     gameOver();
-    //c heckCollitionsBullets()
     checkKeys();
 
     //2Limpiar Canvas();
@@ -254,9 +299,13 @@ function update() {
     //3Dibujas
     board.draw();
     solider.draw();
-    printBullets();
+    drawBullets();
     drawZombies();
+    drawZombieBoss();
     drawPowers();
+    live.draw();
+    point.draw();
+
 }
 
 
@@ -272,61 +321,106 @@ function clearCanvas(){
 
 // Funcion para instanciar de manera aleatoria los Zombies
  function generateZombies(){
-    if (frames % 250 === 0) {
+    if (frames % 50 === 0) {
         const limitHeight = $canvas.height;
         //instancia de mi Enemigo
         const zombie1 = new Zombie(random(limitHeight), 70, 70, zombieImage);
-
         zombies.push(zombie1)
     }
  };
 
- // funcion para checar las coliciones de los zombies
- function checkCollitions(){
+ function generateBossZombie(){
+    if (frames % 400 === 0) {
+        const limitHeight = $canvas.height;
+        //instancia de mi Enemigo
+        const zombie2 = new Zombie(random(limitHeight), 90, 90, zombie2Image);
+        zombieBoss.push(zombie2)
+    }
+ };
 
+
+
+
+
+ // Funcion para instanciar de manera aleatoria los Poderes
+ function generatePowers(){
+    if (frames % 1500 === 0) {
+        const limitWidth = $canvas.width;
+        const limitHeight = $canvas.height;
+        //instancia de mi Poder
+        const power = new Power(random(limitWidth), random(limitHeight), 20, 20,powerImage);
+
+        powers.push(power)
+    }
+ };
+
+
+ // funcion para checar las coliciones de los zombies, balas y poderes
+ function checkCollitions(){
     // comprobación colisión zombies con soldados
     zombies.forEach(obs => {
          if (solider.isTouching(obs)){
            zombies.splice(0, 1)
-          lives--;
+          lives--;          
        }
        else {
         gameOver()
        }
     })
-    
     // comprobación colisión balas con zombies
     zombies.forEach(obs => {
         bullets.forEach(bull => {
             if(bull.isTouching(obs)){
 
-                points++;
                 zombies.splice(0, 1);
-                bullets.splice(0,1);
+                bullets.splice(0, 1);
+
+                points++;
             }
         })
    })
 
-   // Comprobación Colisión Soldado Con poderes
-   powers.forEach(obs => {
+   // comprobación colisión Boss con soldados
+   zombieBoss.forEach(obs => {
     if (solider.isTouching(obs)){
-        powers.splice(0, 1)
-     lives++;
+      zombieBoss.splice(0, 1)
+     lives--;          
   }
-  
+  else {
+   gameOver()
+  }
 })
 
+// comprobación colisión balas con zombies
+zombieBoss.forEach(obs => {
+   bullets.forEach(bull => {
+       if(bull.isTouching(obs)){
+
+           zombieBoss.splice(0, 1);
+           bullets.splice(0, 1);
+
+           (points++) ;
+       }
+   })
+})
+
+
+   // Comprobación Colisión Soldado Con poderes
+   powers.forEach((obs) => {
+     if (solider.isTouching(obs)) {
+       powers.splice(0, 1);
+       lives++;
+     }
+   });
  }
 
 
-
+// Funcion GameOver
  function gameOver(){
     if(lives === 0){
         clearInterval(intervalId);
     }
 }
-
- 
 
  // Funcion para imprimir la instancia aleatoria de los zombies
  function drawZombies(){
@@ -335,17 +429,18 @@ function clearCanvas(){
      });
  }
 
- // Funcion para instanciar de manera aleatoria los Poderes
- function generatePowers(){
-    if (frames % 1000 === 0) {
-        const limitWidth = $canvas.width;
-        const limitHeight = $canvas.height;
-        //instancia de mi Poder
-        const power1 = new Power(random(limitWidth), random(limitHeight), 20, 20,powerImage);
+ function drawZombieBoss(){
+    zombieBoss.forEach((boss) => {
+        boss.draw()
+    });
+}
 
-        powers.push(power1)
-    }
- };
+ // Funcion auxiliar para pintar la bala
+ function drawBullets(){
+    // un for each para recorrer el arreglo y mandar llamar su metedo de impresion por cada una de las balas
+    bullets.forEach((bullet) => bullet.draw());
+};
+
 // Funcion para imprimir la instancia aleatoria de los poderes
  function drawPowers(){
     powers.forEach((power) => {
@@ -353,7 +448,9 @@ function clearCanvas(){
     });
 }
 
- 
+
+
+
 
  //Funcion auxiliar para detectar las multiples teclas, esta verificando constantemente que teclas estan activas con el if
  function checkKeys(){
@@ -362,22 +459,26 @@ function clearCanvas(){
      if(keys.ArrowUp) solider.moveUp();
      if(keys.ArrowDown) solider.moveDown();
 
-     //Codigo para imprimir balas si se deja precionada la tecla
-    /* //  Vamos a definir la tecla que se va a utilizar para disparar (creacion de nuestra bala)
+    //Codigo para imprimir balas si se deja precionada la tecla
+     //  Vamos a definir la tecla que se va a utilizar para disparar (creacion de nuestra bala)
     // con el && vamos a limitar la cantidad de balas que vamos a imprimir por ejemplo cada que los frames tenga un multiplo de 16 es las veces que va a disparar
-    if(keys.e && frames % 15 === 0) {
+    /* if(keys.s && frames % 40 === 0) {
         //Instancia de mi bala
-        const bullet = new Bullet(solider.x,solider.y+15, 10, 10, bulletImage);
+        const bullet = new Bullet(solider.x + 55, solider.y + 35 , 10, 10, bulletImage);
         // la vamos a ingresar en nuestro arreglo que ya habiamos creado.
         bullets.push(bullet);
    } */
  }
 
- // Funcion auxiliar para pintar la bala
- function printBullets(){
-     // un for each para recorrer el arreglo y mandar llamar su metedo de impresion por cada una de las balas
-     bullets.forEach((bullet) => bullet.draw());
- };
+ // Cuando alguien deje de precionar la tecla que se detenga
+
+document.onkeyup = (event) => {
+    //Si alguien deja de precionar la tecla vuelvela falso
+    keys[event.key] = false;
+    //si se deja de precionar la tecla deneter el soldado
+    solider.stop();
+};
+
 
 
 // Funciones de interacción con el usuario.
@@ -388,20 +489,16 @@ document.onkeydown = (event) => {
 
     //Codigo para imprimir balas cada vez que se precione la tecla e
     // se va poner el meteodo para las balas en esta zona para que suceda cada que se preciona la tecla e y no se generen si se deja precionado.
-    if(event.key === "s") {
-        const bullet = new Bullet(solider.x,solider.y+15, 10, 10, bulletImage);
+     if(event.key === "s") {
+        const bullet = new Bullet(solider.x + 55, solider.y + 35 , 10, 10, bulletImage);
         // la vamos a ingresar en nuestro arreglo que ya habiamos creado.
         bullets.push(bullet);
-    }
+    } 
 };
 
-// Cuando alguien deje de precionar la tecla que se detenga
 
-document.onkeyup = (event) => {
-    //Si alguien deja de precionar la tecla vuelvela falso
-    keys[event.key] = false;
-    //si se deja de precionar la tecla deneter el soldado
-    solider.stop();
-};
+ $button.onclick = start();
 
-$button.onclick = start();
+// $button.addEventListener ("click",event => {
+//     start();
+// })
